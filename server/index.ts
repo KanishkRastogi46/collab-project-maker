e6982964efdc32b8429df265ec6f761c10f23c2d
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2025-01-02 00:14:09 +0530

    integrated gen ai model

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,7 @@ import { createServer } from 'http';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from "morgan";
+import model from './gemini.config';
 
 const app = express();
 
@@ -21,6 +22,17 @@ app.use(logger("dev"));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/generate', async (req, res): Promise<any> => {
+  const { text } = req.body;
+  if (!text) {
+    return res.status(400).json({ message: 'Text is required' });
+  }
+
+  const response = await model.generateContent(text);
+  console.log(response.response.text());
+  return res.json({response: response.response.text()});
 });
 
 server.listen(port, () => {
